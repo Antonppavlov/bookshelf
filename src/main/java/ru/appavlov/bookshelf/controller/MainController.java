65f6e62dc731389002b0f4c7d0bd44edaa601218
antonpavlov <anton.it.pavlov@gmail.com>
2018-02-22 14:33:12 +0300

    создал ветку rest и в ней: почистил pom он фронт либ удалил все отвечающее за фронт удалил все отвечающее за локазизацию добавил настройку port

--- src/main/java/ru/appavlov/bookshelf/controller/MainController.java
+++ src/main/java/ru/appavlov/bookshelf/controller/MainController.java
@@ -4,17 +4,17 @@ import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log
 @Controller
 public class MainController {
 
-    // для перенаправления с корня проекта на страницу index.xhtml
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String baseUrlRedirect(HttpServletRequest request, HttpServletResponse httpServletResponse) {
-        return "redirect:" + request.getRequestURL().append("index.xhtml").toString();
+    public @ResponseBody
+    String baseUrlRedirect() {
+        return "Hello!";
     }
 }
+
+
